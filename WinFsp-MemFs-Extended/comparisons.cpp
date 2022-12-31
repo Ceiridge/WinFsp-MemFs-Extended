b94ef805f4b1838d549b4928dd4761b16d44ab11
Ceiridge <philipp34533@icloud.com>
2022-12-31 19:03:53 +0100

    Add new efficient file name comparison methods

--- WinFsp-MemFs-Extended/comparisons.cpp
+++ WinFsp-MemFs-Extended/comparisons.cpp
@@ -2,6 +2,49 @@
 #include "comparisons.h"
 
 namespace Memfs::Utils {
+	static inline unsigned UpperChar(const unsigned c)
+	{
+		/*
+		 * Bit-twiddling upper case char:
+		 *
+		 * - Let signbit(x) = x & 0x100 (treat bit 0x100 as "signbit").
+		 * - 'A' <= c && c <= 'Z' <=> s = signbit(c - 'A') ^ signbit(c - ('Z' + 1)) == 1
+		 *     - c >= 'A' <=> c - 'A' >= 0      <=> signbit(c - 'A') = 0
+		 *     - c <= 'Z' <=> c - ('Z' + 1) < 0 <=> signbit(c - ('Z' + 1)) = 1
+		 * - Bit 0x20 = 0x100 >> 3 toggles uppercase to lowercase and vice-versa.
+		 *
+		 * This is actually faster than `(c - 'a' <= 'z' - 'a') ? (c & ~0x20) : c`, even
+		 * when compiled using cmov conditional moves at least on this system (i7-1065G7).
+		 *
+		 * See https://godbolt.org/z/ebv131Wrh
+		 */
+		const unsigned s = ((c - 'a') ^ (c - ('z' + 1))) & 0x100;
+		return c & ~(s >> 3);
+	}
+
+	static inline int EfficientWcsnicmp(const wchar_t* s0, const wchar_t* t0, int n)
+	{
+		/* Use fast loop for ASCII and fall back to CompareStringW for general case. */
+		const wchar_t* s = s0;
+		const wchar_t* t = t0;
+		int v = 0;
+		for (const void* e = t + n; e > (const void*)t; ++s, ++t)
+		{
+			unsigned sc = *s, tc = *t;
+			if (0xffffff80 & (sc | tc))
+			{
+				v = CompareStringW(LOCALE_INVARIANT, NORM_IGNORECASE, s0, n, t0, n);
+				if (0 != v)
+					return v - 2;
+				else
+					return _wcsnicmp(s, t, n);
+			}
+			if (0 != (v = UpperChar(sc) - UpperChar(tc)) || !tc)
+				break;
+		}
+		return v;/*(0 < v) - (0 > v);*/
+	}
+
 	int FileNameCompare(const PCWSTR a, int alen, const PCWSTR b, int blen, const BOOLEAN caseInsensitive) {
 		PCWSTR p, endp, partp, q, endq, partq;
 		WCHAR c, d;
@@ -41,11 +84,7 @@ namespace Memfs::Utils {
 			len = plen < qlen ? plen : qlen;
 
 			if (caseInsensitive) {
-				res = CompareStringW(LOCALE_INVARIANT, NORM_IGNORECASE, partp, plen, partq, qlen);
-				if (0 != res)
-					res -= 2;
-				else
-					res = _wcsnicmp(partp, partq, len);
+				res = EfficientWcsnicmp(partp, partq, len);
 			} else
 				res = wcsncmp(partp, partq, len);
 
